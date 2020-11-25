3e50642fec63b02212663a2c1391924caced4a9e
Reid Smith <smithreid001@gmail.com>
2020-11-25 09:57:13 -0500

    tweaks for large render

--- script.py
+++ script.py
@@ -104,7 +104,7 @@ def render_frame(x_center, y_center, initial_resolution, n_pixels, max_iter, fra
         saves the frame to disk
     """
     # change values specific to the frame
-    iter_step = 175
+    iter_step = 250
     resolution = initial_resolution * (size_per_frame ** (frame_number - 1))
     max_iter = max_iter + (iter_step * frame_number)
 
@@ -139,10 +139,10 @@ if __name__ == "__main__":
 
     x_center = -0.74943
     y_center = 0.04955
-    resolution = 1.0 / (10 ** 3) # Start at 1 / (10 ** 2)
-    n_pixels = 256
-    max_iter = 1000 # start at 250
-    frames = 12
+    resolution = 1.0 / (10 ** 2) # Start at 1 / (10 ** 2)
+    n_pixels = 512
+    max_iter = 500 # start at 250
+    frames = 144
     size_per_frame = 0.25
     
     start_time = datetime.now()
